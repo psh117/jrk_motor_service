f8a8fb8cbc1c5fc457b4bb389e288eaa2100c86d
Suhan Park <psh117@snu.ac.kr>
2020-10-19 21:38:52 +0900

    update for multiple jrks

--- scripts/jrk_server.py
+++ scripts/jrk_server.py
@@ -10,14 +10,18 @@ from jrk_motor_service.srv import JrkCmd, JrkCmdResponse, JrkCmdRequest
 
 max_bound = 1.0
 exe_path = ''
+
+name_serial_dict = {'right_long' : '00281975', 'top_long': '00296377', 'top_short':'00282035'}
 def handle_jrk_srv(req):
-    
+    if req.name == '':
+        req.name = 'right_long'
+    seiral = name_serial_dict[req.name]
     target = int(round((req.target_value*max_bound) * 2047 + 2048))
     if target > 4096:
         target = 4095
     if target < 0:
         target = 0
-    p = os.system(exe_path + ' --target ' + str(target))
+    p = os.system(exe_path + ' --target ' + str(target) + ' -d ' + serial)
     print ('request target:', req.target_value, 'raw_data:', target, 'return:', p)
     res = JrkCmdResponse()
     res.is_succeed = True
@@ -32,8 +36,12 @@ if __name__ == "__main__":
     bin_path = package_path + '/bin/'
     exe_path = bin_path + 'JrkCmd'
     s = rospy.Service('/jrk_cmd', JrkCmd, handle_jrk_srv)
-    print (os.system(exe_path + ' --target 2048'))
-    print (os.system(exe_path + ' --run'))
+    for name in name_serial_dict:
+        serial = name_serial_dict[name]
+        print (os.system(exe_path + ' --target 2048 -d ' + serial))
+        print (os.system(exe_path + ' --run -d ' + serial))
     print ("Ready to set target.")
     rospy.spin()
-    print (os.system(exe_path + ' --stop'))
+    for name in name_serial_dict:
+        serial = name_serial_dict[name]
+        print (os.system(exe_path + ' --stop -d ' + serial))
